89ff2a89f8be8cfcc2ddf448dffec79f959f4093
maktarsis <tarsis.maksym@gmail.com>
2018-05-05 08:50:49 +0300

    delete contacts cmp

--- src/app/location/location.module.ts
+++ src/app/location/location.module.ts
@@ -8,7 +8,6 @@ import { MarkerDirective } from './shared/marker.directive';
 import { MapService } from './shared/map.service';
 import { GeolocationService } from './shared/geolocation.service';
 import { GeocodingService } from './shared/geocoding.service';
-import { ContactsComponent } from './contacts/contacts.component';
 import { MapComponent } from './map/map.component';
 import { LocationResolver } from './location.resolver';
 import { StocklistsComponent } from './stocklists/stocklists.component';
@@ -23,7 +22,6 @@ import { StocklistsComponent } from './stocklists/stocklists.component';
 		GoogleMapComponent,
 		LocationComponent,
 		MarkerDirective,
-		ContactsComponent,
 		MapComponent,
 		StocklistsComponent
 	],
--- src/app/location/location.routes.ts
+++ src/app/location/location.routes.ts
@@ -8,18 +8,12 @@ import { LocationComponent } from './location.component';
 import { MapComponent } from './map/map.component';
 import { LocationResolver } from './location.resolver';
 import { StocklistsComponent } from './stocklists/stocklists.component';
-import { ContactsComponent } from './contacts/contacts.component';
 
 export const routes: Routes = [
 	{
 		path: '',
 		pathMatch: 'full',
 		component: MapComponent
-	},
-	{
-		path: 'contacts',
-		pathMatch: 'full',
-		component: ContactsComponent
 	},
 	{
 		path: 'stocklists',
